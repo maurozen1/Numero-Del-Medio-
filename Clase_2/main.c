493856b66625b0d77b8917a002cc3a6bb49993fe
alumno <alumno>
2018-08-27 14:34:23 -0300

    Ingreso numeros

---
+++ Clase_2/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numeroUno;
+    int numeroDos;
+    int numeroTres;
+
+
+    printf("Ingrese numero:");
+    scanf("%d",&numeroUno);
+
+    printf("Ingrese numero: ");
+    scanf("%d",&numeroDos);
+
+    printf("Ingrese numero:");
+    scanf("%d",&numeroTres);
+
+
+
+
+
+    return 0;
+}
