9922f0a3fe60279ad44945438ebda62c9ec09607
alumno <alumno>
2018-08-27 15:13:54 -0300

    Max y min

--- Clase_2/main.c
+++ Clase_2/main.c
@@ -6,18 +6,49 @@ int main()
     int numeroUno;
     int numeroDos;
     int numeroTres;
+    int maximo;
+    int minimo;
+    int numeroDelMedio;
 
 
     printf("Ingrese numero:");
     scanf("%d",&numeroUno);
 
+    maximo = numeroUno;
+    minimo = numeroUno;
+
     printf("Ingrese numero: ");
     scanf("%d",&numeroDos);
+
+    if(numeroDos < minimo)
+    {
+        minimo = numeroDos;
+    }else
+    {
+        if ( numeroDos > maximo)
+        {
+            maximo=numeroDos;
+        }
+    }
 
     printf("Ingrese numero:");
     scanf("%d",&numeroTres);
 
+    if(numeroTres < minimo)
+    {
+        minimo = numeroTres;
+    }else
+    {
+        if ( numeroTres > maximo)
+        {
+            maximo=numeroTres;
+        }
+    }
 
+    if(numeroDelMedio > minimo & numeroDelMedio < maximo)
+    {
+
+    }
 
 
 
